70df0bb9c8396f683c5a2796bd2c3e66cde42872
Samir <samirb80440@gmail.com>
2024-03-18 15:37:25 +0100

    commit

--- js/exercice.js
+++ js/exercice.js
@@ -1,12 +1,8 @@
-var prenom,nom;
-prenom=prompt("Ecrire prénom");
+var prenom,nom; 
+prenom=prompt("Ecrire prénom"); 
+ while ( prenom <=5) 
+ {    console.log("le nombre de prenom est de"+prenom+"est"+nom)          
+ prenom++; 
 
-while ( prenom <=5)
-{
-
-    console.log("le nombre de prenom est de"+prenom+"est"+nom)
-    
-    prenom++;
-}
-console.log("fin de la boucle");
-alert=(nom)
+} 
+console.log("fin de la boucle"); alert=(nom)
