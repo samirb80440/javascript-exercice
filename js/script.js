f526b8c238db90f846b072357b93a346d267b32d
Samir <samirb80440@gmail.com>
2024-03-18 14:36:08 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,43 @@
 // exemple !!!
-var a,b,c ,d
-a=100
-b=100
-c=1.00
-alert("Ceci est un chaînes de caractères :"+a)
-b= a--
-a = +a + +c
-alert(a)
-d=a<b
-alert(d)
+var année,age;
+année=prompt("Ecrire un année")
+age=2024-année
+if (age<18)
+(
+alert("Mineur")
+)
+else if(age>18)
+(
+alert("Majeur")
+)
+
+var a,b,c
+a=prompt("Entrez le premier nombre")
+b=prompt("Entrez le second nombre")
+c=prompt("Opérateur")
+switch(c)
+{
+ case"+":
+    c=alert(+a + +b) ;
+     break;
+   
+
+ case"-":
+   c=alert(a-b) ;
+    break;
+
+
+    case"*":
+    c=alert(a*b) ;
+     break;
+
+  case"/":
+   c=alert(a/b) ;
+   break
+   
+
+  default:
+    c=alert("erreur") ;
+}
+
+
