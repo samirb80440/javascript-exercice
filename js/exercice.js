2a3b4a335b57c3534e98ce113003c508407e8978
Samir <samirb80440@gmail.com>
2024-03-19 09:59:15 +0100

    commit

--- js/exercice.js
+++ js/exercice.js
@@ -1,7 +1,20 @@
-var tableau = ["Paul", "Pierre", "Anne", "Sophie"];
-
-for (var i in tableau) 
+var prenom,n
+var myTableau=[];
+for (var i=0; i<100;i++)
 {
-    console.log(tableau[i]);
+
+
+ myTableau[i]=prompt("prenom")
+if(myTableau[i]=="")
+{
+   break
 }
+}
+for(var i=0;i<myTableau.length-1 ;i++)
+  {console.log("prenom :"+myTableau[i])}
+  
+
+
+
+
 
