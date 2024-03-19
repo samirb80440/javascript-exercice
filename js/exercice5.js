fef24a2c5cdddf29eff2317f2c6b27d3e24f2819
Samir <samirb80440@gmail.com>
2024-03-19 14:25:02 +0100

    commit

--- js/exercice5.js
+++ js/exercice5.js
@@ -1,10 +1,13 @@
-var N,a
+var N,
 N=prompt("saisir texte")
-c=n.substring(i,N)
-for (var i=0;i<=N.length;i++)
+let nbVoyelles=0;
+let Voyelles=["a","e","i","o","u","y"];
+for (let i=0;i<N.length;i++)
+  {
+N.substring(i,i+1)
+console.log(N.substring(i,i+1))
+if(Voyelles.indexOf(nbVoyelles))
 {
-
-
-
-    
-}
+  nbVoyelles++}
+  }
+  console.log(nbVoyelles)
