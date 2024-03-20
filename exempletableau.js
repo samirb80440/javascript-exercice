43ce53267ace0d193fd9c5a279484f667b6751d4
Samir <samirb80440@gmail.com>
2024-03-20 15:03:26 +0100

    commit

--- exempletableau.js
+++ exempletableau.js
@@ -1,5 +1,14 @@
-var myTableau = ["pomme", "poire", "banane", "fraise", "abricot"];
-for (var fruit in myTableau) 
+var i,fruits
+var myTableau=[]
+var nb= myTableau.length;
+fruits=prompt("saisie du  nombre de fruit souhaiter")
+for (var i=0; i<fruits; i++)
 {
-   console.log("Fruit : "+myTableau[fruit]);
+myTableau[1]=prompt("fruits")
 }
+myTableau.sort();
+for(var i=0;i<myTableau.length; i++)
+{
+ console.log("fruits: "+myTableau[i]);
+}
+console.log("les tableau contient"+i+"élement");
