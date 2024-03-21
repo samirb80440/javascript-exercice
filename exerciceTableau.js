1c04e8895722fd25559d7fbade2e7861307a7c64
Samir <samirb80440@gmail.com>
2024-03-21 10:01:51 +0100

    commit

--- exerciceTableau.js
+++ exerciceTableau.js
@@ -1,5 +1,5 @@
-var fruits,myTableau=[]
-fruits=prompt("saisie du nombre de fruit souhaité")
+/*var fruits,myTableau=[]
+//fruits=prompt("saisie du nombre de fruit souhaité")
 for (var i= 0; i< fruits; i++)
 {
 myTableau[i]=prompt("fruits")
@@ -7,4 +7,30 @@ myTableau[i]=prompt("fruits")
 console.log(myTableau)
 
 
+function plus(compteur){
+    compteur++
+    return compteur
+}
+
+let compteur=1;
+compteur= plus(compteur);
+console.log(compteur)
+*/
+function produit(X,Y){
+     X*Y
+     X*X*X
+    return (X*Y)
+}
+let X,Y
+X=prompt("Entrez un nombre")
+Y=prompt("Entrez un multiplicateur")
+X,Y=produit(X,Y)
+console.log(X*X*X)
+console.log(Y)
+
+const image= new Image()
+ image.src="/home/bedrouni/DEV bedrouni/exercice/javascript/papillon.jpg"
+ image.onload= function(){
+document.body.appendChild(image)
+ }
 
--- js/exercice6.js
+++ js/exercice6.js
@@ -1,4 +1,4 @@
-var N,X,Y,Z
+/*var N,X,Y,Z
 N=prompt("Entrez un nombre")
 X=prompt("Entrez un mutiplicateur")
 for(var i =0;i <=N;i++)
@@ -8,3 +8,16 @@ for(var i =0;i <=N;i++)
  Z= N* N*N
  console.log(N*N*N)
  Y=prompt("Le produit de "+N*+X,"est égal à"+Y)
+
+
+
+function produit(X,Y){
+    X*Y
+    X*X*X
+    return X,Y
+}
+let X=5
+let Y=2
+X,Y=produit(X,Y)
+console.log(X,Y)
+*/
