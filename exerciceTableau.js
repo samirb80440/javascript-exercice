237c7e458f322483d6021585989901edfc9d0bda
Samir <samirb80440@gmail.com>
2024-03-21 10:14:03 +0100

    commit

--- exerciceTableau.js
+++ exerciceTableau.js
@@ -15,7 +15,7 @@ function plus(compteur){
 let compteur=1;
 compteur= plus(compteur);
 console.log(compteur)
-*/
+
 function produit(X,Y){
      X*Y
      X*X*X
@@ -27,10 +27,16 @@ Y=prompt("Entrez un multiplicateur")
 X,Y=produit(X,Y)
 console.log(X*X*X)
 console.log(Y)
+calcul=X*X*X
+cube="Le cube de"+X+"est égal à "+calcul
+calcul=X*Y
+résultat="Le prduit de"+X*Y+"est égal à "+calcul
+let texte=
 
 const image= new Image()
  image.src="/home/bedrouni/DEV bedrouni/exercice/javascript/papillon.jpg"
  image.onload= function(){
 document.body.appendChild(image)
  }
+ */
 
