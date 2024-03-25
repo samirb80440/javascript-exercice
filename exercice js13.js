e21c86698ebd86242e5f77d5eb0ee983e5687578
Samir <samirb80440@gmail.com>
2024-03-25 11:32:37 +0100

    commit

--- exercice js13.js
+++ exercice js13.js
@@ -1,7 +1,21 @@
-var element = document.getElementById("button1")
+/*var element = document.getElementById("button1")
 
 element.addEventListener("click", function fenetre() {
 
-
- alert("Vous avez saisi ");
+ tex= document.getElementById("textbox").value
+ alert("Vous avez saisi "+tex);
 })
+*/
+
+function math(){
+    var n= Math.random(1,20)
+    tex= document.getElementById("textbox").value
+    alert("nombre"+tex)
+}
+function verif(){
+    if(tex>20)
+    alert("trop grand")
+    else(tex<1)
+    alert("trop petit")
+   
+}
